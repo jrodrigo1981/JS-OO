1bbd02642ef00a15898d81c053964c8bd506325d
jrodrigo1981 <jrodrigo1981@gmail.com>
2021-10-07 23:13:43 -0300

    Criado a classe Funcionarios e seus filhos, no qual foi implementado o Polimorfismo

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-import {Cliente} from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { SistemaAutenticacao } from "./Funcionarios/SistemaAutenticacao.js";
 
 
+const diretor = new Diretor("Rodrigo", 10000, 1245789631);
+const gerente = new Gerente("Ricardo", 5000, 85296374123);
 
-const cliente1 = new Cliente(`Ricardo`, 12345678912);
-const conta1 = new ContaCorrente(1001, cliente1, 1001);
+diretor.cadastrarSenha("123456");
+gerente.cadastrarSenha("123");
 
+const estaLogado = SistemaAutenticacao.login(diretor, "123456");
+const estaLogado1 = SistemaAutenticacao.login(gerente, "123");
 
-const valorDepositado = conta1.depositar(500);
-const valorSacado = conta1.sacar(100);
+console.log(estaLogado);
+console.log(estaLogado1);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-
-console.log(conta1);
-
-console.log(contaPoupanca);
-
-const contasalario = new ContaSalario(cliente1);
-contasalario.depositar(200);
-contasalario.sacar(50);
-
-console.log(contasalario);
